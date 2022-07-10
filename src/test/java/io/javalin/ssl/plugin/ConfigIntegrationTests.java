142721a927301410f5b2f1f58f175b398dd1c796
Alberto Zugazagoitia <alberto@zugazagoitia.com>
2022-07-10 12:53:36 +0200

    Use unique ports for integration testing

--- src/test/java/io/javalin/ssl/plugin/ConfigIntegrationTests.java
+++ src/test/java/io/javalin/ssl/plugin/ConfigIntegrationTests.java
@@ -7,48 +7,55 @@ import okhttp3.Request;
 import okhttp3.Response;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.parallel.ResourceLock;
 
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.parallel.ResourceAccessMode.READ_WRITE;
 
 @Tag("integration")
 public class ConfigIntegrationTests extends IntegrationTestClass {
 
     @Test
-    @ResourceLock(value = "port80", mode = READ_WRITE)
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void testDisableInsecure() {
         OkHttpClient client = createHttpsClient();
+        int insecurePort = ports.getAndIncrement();
+        int securePort = ports.getAndIncrement();
+        String http = HTTP_URL_WITH_PORT.apply(insecurePort);
+        String https = HTTPS_URL_WITH_PORT.apply(securePort);
         try (Javalin app = IntegrationTestClass.createTestApp(config -> {
             config.disableInsecure = true;
             config.loadPemFromString(CERTIFICATE_AS_STRING, NON_ENCRYPTED_KEY_AS_STRING);
+            config.sslPort = securePort;
+            config.insecurePort = insecurePort;
         })) {
             app.start();
-            assertThrows(Exception.class, () -> client.newCall(new Request.Builder().url(HTTP_URL).build()).execute()); // should throw exception
-            Response response = client.newCall(new Request.Builder().url(HTTPS_URL).build()).execute(); // should not throw exception
+            assertThrows(Exception.class, () -> client.newCall(new Request.Builder().url(http).build()).execute()); // should throw exception
+            Response response = client.newCall(new Request.Builder().url(https).build()).execute(); // should not throw exception
             assertEquals(200, response.code());
             assertEquals(SUCCESS, Objects.requireNonNull(response.body()).string());
+            app.stop();
         } catch (IOException e) {
             fail(e);
         }
     }
 
     @Test
-    @ResourceLock(value = "port80", mode = READ_WRITE)
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void testDisableSecure() {
         OkHttpClient client = createHttpsClient();
+        int insecurePort = ports.getAndIncrement();
+        int securePort = ports.getAndIncrement();
+        String http = HTTP_URL_WITH_PORT.apply(insecurePort);
+        String https = HTTPS_URL_WITH_PORT.apply(securePort);
         try (Javalin app = IntegrationTestClass.createTestApp(config -> {
             config.disableSecure = true;
+            config.insecurePort = insecurePort;
+            config.sslPort = securePort;
         })) {
             app.start();
-            assertThrows(Exception.class, () -> client.newCall(new Request.Builder().url(HTTPS_URL).build()).execute()); // should throw exception
-            Response response = client.newCall(new Request.Builder().url(HTTP_URL).build()).execute(); // should not throw exception
+            assertThrows(Exception.class, () -> client.newCall(new Request.Builder().url(https).build()).execute()); // should throw exception
+            Response response = client.newCall(new Request.Builder().url(http).build()).execute(); // should not throw exception
             assertEquals(200, response.code());
             assertEquals(SUCCESS, Objects.requireNonNull(response.body()).string());
         } catch (IOException e) {
@@ -57,16 +64,14 @@ public class ConfigIntegrationTests extends IntegrationTestClass {
     }
 
     @Test
-    @ResourceLock(value = "port12345", mode = READ_WRITE)
     void testInsecurePortChange() {
         OkHttpClient client = createHttpsClient();
         try (Javalin app = IntegrationTestClass.createTestApp(config -> {
             config.disableSecure = true;
-            config.insecurePort = 12345;
+            config.insecurePort = 8080;
         })) {
             app.start();
-            assertThrows(Exception.class, () -> client.newCall(new Request.Builder().url(HTTP_URL).build()).execute()); // should throw exception
-            Response response = client.newCall(new Request.Builder().url("http://localhost:12345/").build()).execute(); // should not throw exception
+            Response response = client.newCall(new Request.Builder().url("http://localhost:8080/").build()).execute(); // should not throw exception
             assertEquals(200, response.code());
             assertEquals(SUCCESS, Objects.requireNonNull(response.body()).string());
         } catch (IOException e) {
@@ -75,17 +80,15 @@ public class ConfigIntegrationTests extends IntegrationTestClass {
     }
 
     @Test
-    @ResourceLock(value = "port12345", mode = READ_WRITE)
     void testSecurePortChange() {
         OkHttpClient client = createHttpsClient();
         try (Javalin app = IntegrationTestClass.createTestApp(config -> {
             config.disableInsecure = true;
             config.loadPemFromString(CERTIFICATE_AS_STRING, NON_ENCRYPTED_KEY_AS_STRING);
-            config.sslPort = 12345;
+            config.sslPort = 8443;
         })) {
             app.start();
-            assertThrows(Exception.class, () -> client.newCall(new Request.Builder().url(HTTPS_URL).build()).execute()); // should throw exception
-            Response response = client.newCall(new Request.Builder().url("https://localhost:12345/").build()).execute(); // should not throw exception
+            Response response = client.newCall(new Request.Builder().url("https://localhost:8443/").build()).execute(); // should not throw exception
             assertEquals(200, response.code());
             assertEquals(SUCCESS, Objects.requireNonNull(response.body()).string());
         } catch (IOException e) {
@@ -94,15 +97,17 @@ public class ConfigIntegrationTests extends IntegrationTestClass {
     }
 
     @Test
-    @ResourceLock(value = "port80", mode = READ_WRITE)
     void testInsecureHttp1() {
         OkHttpClient client = new OkHttpClient.Builder().build();
+        int insecurePort = ports.getAndIncrement();
+        String http = HTTP_URL_WITH_PORT.apply(insecurePort);
         try (Javalin app = IntegrationTestClass.createTestApp(config -> {
             config.disableSecure = true;
             config.disableHttp2 = true;
+            config.insecurePort = insecurePort;
         })) {
             app.start();
-            Response response = client.newCall(new Request.Builder().url(HTTP_URL).build()).execute();
+            Response response = client.newCall(new Request.Builder().url(http).build()).execute();
             assertEquals(200, response.code());
             assertEquals(SUCCESS, Objects.requireNonNull(response.body()).string());
             assertEquals(Protocol.HTTP_1_1, response.protocol());
@@ -113,20 +118,19 @@ public class ConfigIntegrationTests extends IntegrationTestClass {
     }
 
     @Test
-    @ResourceLock(value = "port80", mode = READ_WRITE)
     void testInsecureDisableHttp2() {
-        OkHttpClient http2client = new OkHttpClient.Builder()
-            .protocols(Collections.singletonList(Protocol.H2_PRIOR_KNOWLEDGE))
-            .build();
-        OkHttpClient http1Client = new OkHttpClient.Builder()
-            .build();
+        OkHttpClient http2client = new OkHttpClient.Builder().protocols(Collections.singletonList(Protocol.H2_PRIOR_KNOWLEDGE)).build();
+        OkHttpClient http1Client = new OkHttpClient.Builder().build();
+        int insecurePort = ports.getAndIncrement();
+        String http = HTTP_URL_WITH_PORT.apply(insecurePort);
         try (Javalin app = IntegrationTestClass.createTestApp(config -> {
             config.disableSecure = true;
             config.disableHttp2 = true;
+            config.insecurePort = insecurePort;
         })) {
             app.start();
-            assertThrows(Exception.class, () -> http2client.newCall(new Request.Builder().url(HTTP_URL).build()).execute()); // Should fail to connect using HTTP/2
-            Response response = http1Client.newCall(new Request.Builder().url(HTTP_URL).build()).execute(); // Should connect using HTTP/1.1
+            assertThrows(Exception.class, () -> http2client.newCall(new Request.Builder().url(http).build()).execute()); // Should fail to connect using HTTP/2
+            Response response = http1Client.newCall(new Request.Builder().url(http).build()).execute(); // Should connect using HTTP/1.1
             assertEquals(200, response.code());
             assertEquals(SUCCESS, Objects.requireNonNull(response.body()).string());
             assertEquals(Protocol.HTTP_1_1, response.protocol());
@@ -136,15 +140,19 @@ public class ConfigIntegrationTests extends IntegrationTestClass {
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void testSecureDisableHttp2() {
         OkHttpClient client = createHttpsClient();
+        int insecurePort = ports.getAndIncrement();
+        int securePort = ports.getAndIncrement();
+        String https = HTTPS_URL_WITH_PORT.apply(securePort);
         try (Javalin app = IntegrationTestClass.createTestApp(config -> {
             config.disableHttp2 = true;
             config.loadPemFromString(CERTIFICATE_AS_STRING, NON_ENCRYPTED_KEY_AS_STRING);
+            config.sslPort = securePort;
+            config.insecurePort = insecurePort;
         })) {
             app.start();
-            Response response = client.newCall(new Request.Builder().url(HTTPS_URL).build()).execute();
+            Response response = client.newCall(new Request.Builder().url(https).build()).execute();
             assertEquals(200, response.code());
             assertEquals(SUCCESS, Objects.requireNonNull(response.body()).string());
             assertEquals(Protocol.HTTP_1_1, response.protocol());
@@ -155,17 +163,20 @@ public class ConfigIntegrationTests extends IntegrationTestClass {
     }
 
     @Test
-    @ResourceLock(value = "port80", mode = READ_WRITE)
     void testInsecureHttp2() {
-        OkHttpClient client = new OkHttpClient.Builder()
-            .protocols(Collections.singletonList(Protocol.H2_PRIOR_KNOWLEDGE))
-            .build();
+        OkHttpClient client = new OkHttpClient.Builder().protocols(Collections.singletonList(Protocol.H2_PRIOR_KNOWLEDGE)).build();
+        int insecurePort = ports.getAndIncrement();
+        int securePort = ports.getAndIncrement();
+        String http = HTTP_URL_WITH_PORT.apply(insecurePort);
+
         try (Javalin app = IntegrationTestClass.createTestApp(config -> {
             config.disableSecure = true;
             config.disableHttp2 = false;
+            config.insecurePort = insecurePort;
+            config.sslPort = securePort;
         })) {
             app.start();
-            Response response = client.newCall(new Request.Builder().url(HTTP_URL).build()).execute();
+            Response response = client.newCall(new Request.Builder().url(http).build()).execute();
             assertEquals(200, response.code());
             assertEquals(SUCCESS, Objects.requireNonNull(response.body()).string());
             assertEquals(Protocol.H2_PRIOR_KNOWLEDGE, response.protocol());
@@ -176,15 +187,48 @@ public class ConfigIntegrationTests extends IntegrationTestClass {
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void testSecureHttp2() {
         OkHttpClient client = createHttpsClient();
-        try (Javalin app = IntegrationTestClass.createTestApp(config -> config.loadPemFromString(CERTIFICATE_AS_STRING, NON_ENCRYPTED_KEY_AS_STRING));) {
+        int insecurePort = ports.getAndIncrement();
+        int securePort = ports.getAndIncrement();
+        String https = HTTPS_URL_WITH_PORT.apply(securePort);
+        try (Javalin app = IntegrationTestClass.createTestApp(config -> {
+            config.loadPemFromString(CERTIFICATE_AS_STRING, NON_ENCRYPTED_KEY_AS_STRING);
+            config.sslPort = securePort;
+            config.insecurePort = insecurePort;
+        })) {
             app.start();
-            Response response = client.newCall(new Request.Builder().url(HTTPS_URL).build()).execute();
+            Response response = client.newCall(new Request.Builder().url(https).build()).execute();
             assertEquals(200, response.code());
             assertEquals(SUCCESS, Objects.requireNonNull(response.body()).string());
             assertEquals(Protocol.HTTP_2, response.protocol());
+            response.close();
+        } catch (IOException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void testDefault() {
+        OkHttpClient client = createHttpsClient();
+        int insecurePort = ports.getAndIncrement();
+        int securePort = ports.getAndIncrement();
+        String http = HTTP_URL_WITH_PORT.apply(insecurePort);
+        String https = HTTPS_URL_WITH_PORT.apply(securePort);
+        try (Javalin app = IntegrationTestClass.createTestApp(config -> {
+            config.insecurePort = insecurePort;
+            config.sslPort = securePort;
+            config.loadPemFromString(CERTIFICATE_AS_STRING, NON_ENCRYPTED_KEY_AS_STRING);
+        })) {
+            app.start();
+            Response response = client.newCall(new Request.Builder().url(http).build()).execute();
+            assertEquals(200, response.code());
+            assertEquals(SUCCESS, Objects.requireNonNull(response.body()).string());
+            assertEquals(Protocol.HTTP_1_1, response.protocol());
+            Response response2 = client.newCall(new Request.Builder().url(https).build()).execute();
+            assertEquals(200, response2.code());
+            assertEquals(SUCCESS, Objects.requireNonNull(response2.body()).string());
+            assertEquals(Protocol.HTTP_2, response2.protocol());
             response.close();
         } catch (IOException e) {
             fail(e);
--- src/test/java/io/javalin/ssl/plugin/PemLoadingTests.java
+++ src/test/java/io/javalin/ssl/plugin/PemLoadingTests.java
@@ -5,132 +5,110 @@ import nl.altindag.ssl.exception.GenericIOException;
 import nl.altindag.ssl.exception.PrivateKeyParseException;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.parallel.ResourceLock;
 
 import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.parallel.ResourceAccessMode.READ_WRITE;
 
 @Tag("integration")
 public class PemLoadingTests extends IntegrationTestClass {
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadValidPasswordlessFromString() {
         assertSslWorks(config -> config.loadPemFromString(CERTIFICATE_AS_STRING, NON_ENCRYPTED_KEY_AS_STRING));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadInvalidKeyFromString() {
         assertThrows(PrivateKeyParseException.class, () -> assertSslWorks(config -> config.loadPemFromString(CERTIFICATE_AS_STRING, "invalid")));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadInvalidCertificateFromString() {
         assertThrows(CertificateParseException.class, () -> assertSslWorks(config -> config.loadPemFromString("invalid", NON_ENCRYPTED_KEY_AS_STRING)));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadInvalidPasswordFromString() {
         assertThrows(PrivateKeyParseException.class, () -> assertSslWorks(config -> config.loadPemFromString(CERTIFICATE_AS_STRING, ENCRYPTED_KEY_AS_STRING, "invalid")));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadValidEncryptedFromString() {
-        assertSslWorks(config -> config.loadPemFromString(CERTIFICATE_AS_STRING, ENCRYPTED_KEY_AS_STRING,KEY_PASSWORD));
+        assertSslWorks(config -> config.loadPemFromString(CERTIFICATE_AS_STRING, ENCRYPTED_KEY_AS_STRING, KEY_PASSWORD));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadValidPasswordlessFromClasspath() {
         assertSslWorks(config -> config.loadPemFromClasspath(CERTIFICATE_FILE_NAME, NON_ENCRYPTED_KEY_FILE_NAME));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadValidEncryptedFromClasspath() {
-        assertSslWorks(config -> config.loadPemFromClasspath(CERTIFICATE_FILE_NAME, ENCRYPTED_KEY_FILE_NAME,KEY_PASSWORD));
+        assertSslWorks(config -> config.loadPemFromClasspath(CERTIFICATE_FILE_NAME, ENCRYPTED_KEY_FILE_NAME, KEY_PASSWORD));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadInvalidPasswordFromClasspath() {
         assertThrows(PrivateKeyParseException.class, () -> assertSslWorks(config -> config.loadPemFromClasspath(CERTIFICATE_FILE_NAME, ENCRYPTED_KEY_FILE_NAME, "invalid")));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadInvalidCertificateFromClasspath() {
         assertThrows(IllegalArgumentException.class, () -> assertSslWorks(config -> config.loadPemFromClasspath("invalid", NON_ENCRYPTED_KEY_FILE_NAME)));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadInvalidKeyFromClasspath() {
         assertThrows(IllegalArgumentException.class, () -> assertSslWorks(config -> config.loadPemFromClasspath(CERTIFICATE_FILE_NAME, "invalid")));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadValidPasswordlessFromFile() {
         assertSslWorks(config -> config.loadPemFromPath(CERTIFICATE_PATH, NON_ENCRYPTED_KEY_PATH));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadValidEncryptedFromFile() {
-        assertSslWorks(config -> config.loadPemFromPath(CERTIFICATE_PATH, ENCRYPTED_KEY_PATH,KEY_PASSWORD));
+        assertSslWorks(config -> config.loadPemFromPath(CERTIFICATE_PATH, ENCRYPTED_KEY_PATH, KEY_PASSWORD));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadInvalidPasswordFromFile() {
         assertThrows(PrivateKeyParseException.class, () -> assertSslWorks(config -> config.loadPemFromPath(CERTIFICATE_PATH, ENCRYPTED_KEY_PATH, "invalid")));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadInvalidCertificateFromFile() {
         assertThrows(GenericIOException.class, () -> assertSslWorks(config -> config.loadPemFromPath("invalid", NON_ENCRYPTED_KEY_PATH)));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadInvalidKeyFromFile() {
         assertThrows(GenericIOException.class, () -> assertSslWorks(config -> config.loadPemFromPath(CERTIFICATE_PATH, "invalid")));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadValidPasswordlessFromInputStream() {
         assertSslWorks(config -> config.loadPemFromInputStream(CERTIFICATE_INPUT_STREAM_SUPPLIER.get(), NON_ENCRYPTED_KEY_INPUT_STREAM_SUPPLIER.get()));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadValidEncryptedFromInputStream() {
-        assertSslWorks(config -> config.loadPemFromInputStream(CERTIFICATE_INPUT_STREAM_SUPPLIER.get(), ENCRYPTED_KEY_INPUT_STREAM_SUPPLIER.get(),KEY_PASSWORD));
+        assertSslWorks(config -> config.loadPemFromInputStream(CERTIFICATE_INPUT_STREAM_SUPPLIER.get(), ENCRYPTED_KEY_INPUT_STREAM_SUPPLIER.get(), KEY_PASSWORD));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadInvalidPasswordFromInputStream() {
         assertThrows(PrivateKeyParseException.class, () -> assertSslWorks(config -> config.loadPemFromInputStream(CERTIFICATE_INPUT_STREAM_SUPPLIER.get(), ENCRYPTED_KEY_INPUT_STREAM_SUPPLIER.get(), "invalid")));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadInvalidCertificateFromInputStream() {
-        assertThrows(CertificateParseException.class, () -> assertSslWorks(config -> config.loadPemFromInputStream(InputStream.nullInputStream(),NON_ENCRYPTED_KEY_INPUT_STREAM_SUPPLIER.get())));
+        assertThrows(CertificateParseException.class, () -> assertSslWorks(config -> config.loadPemFromInputStream(InputStream.nullInputStream(), NON_ENCRYPTED_KEY_INPUT_STREAM_SUPPLIER.get())));
     }
 
     @Test
-    @ResourceLock(value = "port443", mode = READ_WRITE)
     void loadInvalidKeyFromInputStream() {
         assertThrows(PrivateKeyParseException.class, () -> assertSslWorks(config -> config.loadPemFromInputStream(ENCRYPTED_KEY_INPUT_STREAM_SUPPLIER.get(), InputStream.nullInputStream())));
     }
